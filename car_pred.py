0b4d9d596865c0280e5159ac5e52041137ab3069
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2024-11-20 03:19:41 +0530

    Update car_pred.py

--- car_pred.py
+++ car_pred.py
@@ -23,12 +23,14 @@ fuel_type = col1.selectbox("Select the fuel type:", ["Diesel", "Petrol", "CNG", 
 engine = col1.slider("Set the engine power:", 700,5000, step=100)
 transmission_type = col2.selectbox("Select the transmission type:", ["Manual", "Automatic"])
 seats = col2.selectbox("Enter the number of seats:", [4,5,7,9,11])
+model_year = col1.selectbox("Enter the make-Year:", 1980,2024, step=1)
+mileage = col2.number_input("Enter the mileage:")
 
 #Encoding categorical features
 encode_dict = {
     "fuel_type": {'Diesel':1, 'Petrol':2, 'CNG':3, 'LPG':4, 'Electric':5},
     "seller_type": {'Dealer':1, 'Individual':2, 'Trustmark_Dealer':3},
-    "transmission_type": {'Manual':1, 'Automatic':2}
+    "transmission_type": {'Manual':1, 'Automatic':2},
 }
 
 if st.button("Get Price"):
