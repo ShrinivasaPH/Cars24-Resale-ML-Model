29dab37d529935aa025c7cc41bb61c836d75b511
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2024-11-19 22:59:11 +0530

    Update car_pred.py

--- car_pred.py
+++ car_pred.py
@@ -43,4 +43,4 @@ if st.button("Get Price"):
     st.header(round(pred,2))
     st.write("In Lakhs")
 
-st.caption("Disclaimer: This web app was developed as part of an academic project and is for informational purposes only. The predicted resale values are based on a machine learning model and may not accurately reflect real-world market conditions. The developer is not liable for any decisions made based on the app’s output. Users should consult professionals for reliable car resale valuations.")
+st.text("Disclaimer: This web app was developed as part of an academic project and is for informational purposes only. The predicted resale values are based on a machine learning model and may not accurately reflect real-world market conditions. The developer is not liable for any decisions made based on the app’s output. Users should consult professionals for reliable car resale valuations.")
