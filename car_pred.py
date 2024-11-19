e98aef981f6e8b75c4215a037a9576867b5a37eb
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2024-11-20 03:30:09 +0530

    Update car_pred.py

--- car_pred.py
+++ car_pred.py
@@ -24,21 +24,25 @@ engine = col1.slider("Set the engine power:", 700,5000, step=100)
 transmission_type = col2.selectbox("Select the transmission type:", ["Manual", "Automatic"])
 seats = col2.selectbox("Enter the number of seats:", [4,5,7,9,11])
 model_year = col1.selectbox("Enter the make-Year:", 1980,2024, step=1)
-mileage = col2.number_input("Enter the mileage:")
+mileage = col1.slider("Enter the mileage:")
+seller_type = col2.number_input("Enter the seller Type:", [1,2,3])
+km_driven = col2.slider("Enter the mileage:")
+max_power = col1.slider("Enter the max power:")
 
 #Encoding categorical features
 encode_dict = {
     "fuel_type": {'Diesel':1, 'Petrol':2, 'CNG':3, 'LPG':4, 'Electric':5},
     "seller_type": {'Dealer':1, 'Individual':2, 'Trustmark_Dealer':3},
-    "transmission_type": {'Manual':1, 'Automatic':2},
+    "transmission_type": {'Manual':1, 'Automatic':2}
 }
 
 if st.button("Get Price"):
     #call the model.predict() function
     encoded_fuel_type = encode_dict['fuel_type'][fuel_type]
     encoded_transmission_type = encode_dict['transmission_type'][transmission_type]
+    encoded_seller_type = encode_dict["seller_type"][seller_type]
 
-    input_data = [2012.0,2,120000,encoded_fuel_type, encoded_transmission_type,19.7,engine,46.3,seats]
+    input_data = [model_year,encoded_seller_type,km_driven,encoded_fuel_type, encoded_transmission_type,encoded_seller_type,engine,max_power,seats]
 
     pred = model.predict([input_data])[0]
     st.header(":rainbow[Your car's price-prediction below:]")
