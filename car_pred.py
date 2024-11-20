e57b55142f6cfd107c146163a4cfe80357aac4e3
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2024-11-20 20:43:59 +0530

    Update car_pred.py

--- car_pred.py
+++ car_pred.py
@@ -25,7 +25,7 @@ engine = col1.slider("Set the engine power:", 700,5000, step=100)
 transmission_type = col2.selectbox("Select the transmission type:", ["Manual", "Automatic"])
 seats = col2.selectbox("Enter the number of seats:", [4,5,7,9,11])
 km_driven = col1.slider("Select the KM driven:", 100,3800000,step=1)
-year = col2.selectbox("Select the year:", 1992,2021, step=1)
+year = col2.selectbox("Select the year:", 1992,2021)
 mileage =  col1.slider("Select the mileage:", 4,120, step=5)
 
 #Encoding categorical features
